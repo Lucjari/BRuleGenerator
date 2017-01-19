c080ec095a721ccc38bcc5317ef0e66467c25821
JariPC <jari,bloem@live,nl>
2017-01-19 12:09:22 +0100

    Verwijder comments

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import BusinessRule.BusinessRuleService;
  * Created by Kvanwijngaarden on 17-1-2017.
  */
 
-//sdfsdfsdf sdf sdfsd f sdf ads f
-//hallo daar
 public class Main {
 
     public static void main(String args[]){
